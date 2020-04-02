175f761296104cd3b894d34cccf28a5317aec477
tesla59 <talktonishantsingh.ns@gmail.com>
2020-04-02 16:50:39 +0530

    build DTBO

--- kernel.sh
+++ kernel.sh
@@ -1,11 +1,14 @@
-
-# Making out directory
+# Cleaning Everything
+make clean
+make mrproper
+rm -rf out zipper
 mkdir out
 
 # Cloning GCC and CLANG
 git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9 --depth=1 gcc
 git clone https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9 --depth=1 gcc32
 git clone https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86 --depth=1 clang
+git clone https://android.googlesource.com/platform/system/libufdt scripts/ufdt/libufdt
 
 # Some nigga exports
 export KBUILD_BUILD_USER="tesla"
@@ -18,9 +21,14 @@ make O=out mrproper
 make O=out ARCH=arm64 vendor/violet-perf_defconfig
 make -j$(nproc --all) O=out ARCH=arm64 CC="$(pwd)/clang/clang-r370808/bin/clang" CLANG_TRIPLE="aarch64-linux-gnu-" CROSS_COMPILE="$(pwd)/gcc/bin/aarch64-linux-android-" CROSS_COMPILE_ARM32="$(pwd)/gcc32/bin/arm-linux-androideabi-" | tee log.log
 
+# Building DTBO
+python2 "scripts/ufdt/libufdt/utils/src/mkdtboimg.py" \
+create "out/arch/arm64/boot/dtbo.img" --page_size=4096 "out/arch/arm64/boot/dts/qcom/sm6150-idp-overlay.dtbo"
+
 # Refining the kernel
 git clone https://github.com/tesla59/AnyKernel3 zipper
 cp out/arch/arm64/boot/Image.gz-dtb zipper
+cp out/arch/arm64/boot/dtbo.img zipper
 cd zipper
 zip -r9 hydrakernel-$(TZ=Asia/Kolkata date +'%Y%m%d-%H%M').zip * -x README.md hydrakernel-$(TZ=Asia/Kolkata date +'%Y%m%d-%H%M').zip
 
